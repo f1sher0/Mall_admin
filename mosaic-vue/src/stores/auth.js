7c3b4a8750f0051eea521afcd379d47c370740a5
White1943 <whiteyang1943@gmail.com>
2024-05-23 17:31:39 +0800

    微调前端登录,准备进行合并到develop

---
+++ mosaic-vue/src/stores/auth.js
@@ -0,0 +1,37 @@
+// src/stores/auth.js
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import router from '../router';  // 正确导入 router
+import axios from 'axios';
+
+export const useAuthStore = defineStore('auth', () => {
+  const isAuthenticated = ref(!!localStorage.getItem('authToken'));
+
+  async function login(email, password) {
+    try {
+      const response = await axios.post('http://localhost:5052/api/user/login', { email, password });
+      // const token = response.data.token;
+      const token = response.data ;
+	 //弹窗提示,对没有获取token的情况处理
+	 if(token==null){
+	 }
+      console.log(response.data)
+      // const token = "jwtTokenTest"; 
+      localStorage.setItem('authToken', token);
+      alert(localStorage.getItem('authToken'))
+      isAuthenticated.value = true;
+      router.push('/');  // Redirect to the Dashboard
+    } catch (error) {
+      console.error('Login failed:', error);
+      // Handle login error (e.g., show notification)
+    }
+  }
+
+  function logout() {
+    localStorage.removeItem('authToken');
+    isAuthenticated.value = false;
+    router.push('/signin'); // 跳转到登录页面
+  }
+
+  return { isAuthenticated, login, logout };
+});
